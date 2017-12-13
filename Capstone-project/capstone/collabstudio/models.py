2ddec0a7d418b7bc281c6f7cb9404a6e2239b945
Neville Oronni <nevooronni@gmail.com>
2017-12-13 12:18:59 +0300

    created index route and template

--- Capstone-project/capstone/collabstudio/models.py
+++ Capstone-project/capstone/collabstudio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+Gender_Choices = (
+		('Female', 'female'),
+		('Male', 'male'),
+		('Both', 'both'),
+		('None', 'non-specified'),
+	)
+
--- Capstone-project/capstone/collabstudio/views.py
+++ Capstone-project/capstone/collabstudio/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.conf.urls import url 
 from django.http import HttpResponse
 
-def welcome(request):
-	return render(request,'welcome.html')
+def index(request):
+	return render(request,'index.html')
